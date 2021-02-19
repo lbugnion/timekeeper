263f3e29c9a3408b12b162f2324f33aff64abc8b
LBugnion <lbugnion@outlook.com>
2021-02-19 23:22:04 +0100

    Restored original pages and made some improvements to the layout and styles

--- TimekeeperClient/TimekeeperClient/Pages/Index.razor.cs
+++ TimekeeperClient/TimekeeperClient/Pages/Index.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace TimekeeperClient.Pages
 {
@@ -9,6 +12,60 @@ namespace TimekeeperClient.Pages
         {
             get;
             set;
+        }
+
+        public string ClientVersion
+        {
+            get;
+            private set;
+        }
+
+        public string Environment
+        {
+            get;
+            private set;
+        }
+
+        protected override void OnInitialized()
+        {
+            Log.LogInformation("-> Index.OnInitialized");
+            Log.LogDebug($"Session: {Session}");
+
+            if (!string.IsNullOrEmpty(Session))
+            {
+                Nav.NavigateTo($"/guest/{Session}");
+                return;
+            }
+
+            try
+            {
+                var version = Assembly
+                    .GetExecutingAssembly()
+                    .GetName()
+                    .Version;
+
+                Log.LogDebug($"Full version: {version}");
+                ClientVersion = $"V{version.ToString(4)}";
+                Log.LogDebug($"clientVersion: {ClientVersion}");
+
+                var environment = Config.GetValue<string>("Environment");
+                if (environment == "Production")
+                {
+                    environment = string.Empty;
+                }
+
+                Environment = environment;
+            }
+            catch
+            {
+                Log.LogWarning($"Assembly not found");
+                ClientVersion = "N/A";
+            }
+        }
+
+        public void LogInHost()
+        {
+            Nav.NavigateTo("/host");
         }
     }
 }
