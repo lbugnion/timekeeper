b81b7cc9ee0d3907ccbb9f2f3a32a9bbaa04a55d
LBugnion <lbugnion@outlook.com>
2021-02-21 22:14:34 +0100

    Adding a test function to return the user's identity as returned by the identity provider

--- Api/TestOnly.cs
+++ Api/TestOnly.cs
@@ -6,12 +6,51 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Linq;
 
 namespace Timekeeper
 {
     public static class TestOnly
     {
+        private class ClientPrincipal
+        {
+            public string IdentityProvider { get; set; }
+            public string UserId { get; set; }
+            public string UserDetails { get; set; }
+            public IEnumerable<string> UserRoles { get; set; }
+        }
+
+        public static ClaimsPrincipal Parse(HttpRequest req)
+        {
+            var principal = new ClientPrincipal();
+
+            if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
+            {
+                var data = header[0];
+                var decoded = Convert.FromBase64String(data);
+                var json = Encoding.ASCII.GetString(decoded);
+                principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+
+            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+            if (!principal.UserRoles?.Any() ?? true)
+            {
+                return new ClaimsPrincipal();
+            }
+
+            var identity = new ClaimsIdentity(principal.IdentityProvider);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            return new ClaimsPrincipal(identity);
+        }
+
         [FunctionName("TestOnly")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(
@@ -22,7 +61,10 @@ namespace Timekeeper
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("WORKS");
+
+            var identity = Parse(req);
+
+            return new OkObjectResult(identity);
         }
     }
 }
