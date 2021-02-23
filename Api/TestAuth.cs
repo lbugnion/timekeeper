2f65aa59d7e8a9009105870966bbe5323b13ab66
LBugnion <lbugnion@outlook.com>
2021-02-23 22:32:07 +0100

    Trying way to get identity

--- Api/TestAuth.cs
+++ Api/TestAuth.cs
@@ -1,20 +1,29 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
 using System.Linq;
-using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Security.Claims;
+using System.Collections.Generic;
 
 namespace Timekeeper.Api.Free
 {
     public static class AuthTest
     {
+        private class ClientPrincipal
+        {
+            public string IdentityProvider { get; set; }
+            public string UserId { get; set; }
+            public string UserDetails { get; set; }
+            public IEnumerable<string> UserRoles { get; set; }
+        }
+
         [Authorize]
         [FunctionName("AuthTest")]
         public static async Task<IActionResult> Run(
@@ -22,11 +31,34 @@ namespace Timekeeper.Api.Free
             AuthorizationLevel.Anonymous, 
             "get", 
             Route = "test")] 
-        HttpRequest req)
+        HttpRequest req,
+        ILogger log)
         {
-            var client = new HttpClient();
-            var json = await client.GetStringAsync("/.auth/me");
-            return new OkObjectResult(json);
+            var principal = new ClientPrincipal();
+
+            if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
+            {
+                var data = header[0];
+                var decoded = Convert.FromBase64String(data);
+                var json = Encoding.ASCII.GetString(decoded);
+                principal = JsonSerializer.Deserialize<ClientPrincipal>(
+                    json, 
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+
+            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+            if (!principal.UserRoles?.Any() ?? true)
+            {
+                return new OkObjectResult(new ClaimsPrincipal());
+            }
+
+            var identity = new ClaimsIdentity(principal.IdentityProvider);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            return new OkObjectResult(new ClaimsPrincipal(identity));
         }
     }
 }
