094c9639e1756b65b89a54d41f201910a8ce4a2f
LBugnion <lbugnion@outlook.com>
2021-02-23 22:18:30 +0100

    More auth tests

--- Api/TestAuth.cs
+++ Api/TestAuth.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
 using System.Linq;
+using System.Net.Http;
 
 namespace Timekeeper.Api.Free
 {
@@ -17,26 +18,15 @@ namespace Timekeeper.Api.Free
         [Authorize]
         [FunctionName("AuthTest")]
         public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "test")] HttpRequest req)
+        [HttpTrigger(
+            AuthorizationLevel.Anonymous, 
+            "get", 
+            Route = "test")] 
+        HttpRequest req)
         {
-            // Current user identity returned
-            // (quickly thrown together as you get a circular reference error when converting to JSON):
-            return new OkObjectResult(
-               req.HttpContext.User.Identities.Select(x =>
-                  new {
-                      Claims = x.Claims.Select(y => new
-                      {
-                          y.Type,
-                          y.Value,
-                          y.Issuer,
-                          y.Properties
-                      }),
-                      x.Name,
-                      x.Actor,
-                      x.AuthenticationType,
-                      x.NameClaimType
-                  })
-            );
+            var client = new HttpClient();
+            var json = await client.GetStringAsync("/.auth/me");
+            return new OkObjectResult(json);
         }
     }
 }
