edc0666ee2047bd725fed263722756d3435a670d
LBugnion <lbugnion@outlook.com>
2021-03-31 10:02:27 +0200

    Versioning

--- Api/GetVersion.cs
+++ Api/GetVersion.cs
@@ -18,7 +18,7 @@ namespace Timekeeper
             HttpRequest req,
             ILogger log)
         {
-            return new OkObjectResult("0.5.6.2");
+            return new OkObjectResult("0.5.6.3");
         }
     }
 }
