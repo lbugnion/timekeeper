3e4269ac63cfb2ebb2b45a1320dbb0f031d66ad9
LBugnion <lbugnion@outlook.com>
2021-03-31 10:03:52 +0200

    Versioning

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using Timekeeper.Client.Model;
 
 // Set version number for the assembly.
-[assembly: AssemblyVersion("0.5.6.2")]
+[assembly: AssemblyVersion("0.5.6.3")]
 
 namespace Timekeeper.Client
 {
