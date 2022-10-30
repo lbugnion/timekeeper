366a2aceb73f46dad36e28c282181f7973e8e987
Laurent Bugnion <LBugnion@outlook.com>
2022-10-30 12:14:32 +0100

    Improving redirect

--- Client/Pages/LoginProviders.razor.cs
+++ Client/Pages/LoginProviders.razor.cs
@@ -4,15 +4,34 @@ namespace Timekeeper.Client.Pages
 {
     public partial class LoginProviders
     {
+        private const string HostRedirect = "host";
+
+        private string _redirect;
+
         [Parameter]
         public string SessionId { get; set; }
         
         [Parameter]
-        public string Redirect { get; set; }
+        public string Redirect 
+        {
+            get => _redirect;
+            
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _redirect = HostRedirect;
+                }
+                else
+                {
+                    _redirect = value;
+                }
+            }
+        }
 
         public LoginProviders() 
         {
-            Redirect = "host";
+            _redirect = HostRedirect;
         }
     }
 }
